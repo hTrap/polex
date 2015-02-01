b891b9437530d019cd500e545a1ab61445d8ef2b
hTrap <parthoberoi@hotmail.com>
2015-02-02 04:38:57 +0530

    costomizing the admin form

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from polls.models import Question
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
 
 # Register your models here.
+admin.site.register(Question, QuestionAdmin)
+
+
