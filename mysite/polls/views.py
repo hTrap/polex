503f4a3f23dbeabd67fb95d75676caff01722a39
hTrap <parthoberoi@hotmail.com>
2015-02-03 23:34:56 +0530

    removing hard coded urls from template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,18 +1,15 @@
 from polls.models import Question
+from django.shoertcuts import render
 from django.http import HttpResponse
-from django.template import RequestContext, loader
-
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
     
 def results(request, question_id):
     response = "You're lookind at the resluts of question %s."
